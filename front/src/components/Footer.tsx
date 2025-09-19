c45c7e14e75af87cddb3ad4259e07f93ebacf94b
Jonathan <cifuentes.jonathan@gmail.com>
2025-09-19 12:43:53 -0500

    fix: brand logo

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -1,11 +1,15 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <footer className="w-full bg-black border-t border-gray-700">
       <div className="container mx-auto px-4 py-4 flex justify-center">
-        <img
+        <Image
           src="https://cdn.millionluxury.com/image-resizing?image=https://maustorageprod.blob.core.windows.net/spinfileuat/Spin/Data/Estate/IMG/ceb693ad6b7643fc8c1be271d6a9c068.svg"
           alt="Logo"
-          className="h-8 w-auto filter brightness-0 invert"
+          width={120}
+          height={32}
+          unoptimized
         />
       </div>
     </footer>
--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,11 +1,16 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
     <header className="w-full bg-white border-b border-gray-200">
       <div className="container mx-auto px-4 py-4">
-        <img
+        <Image
           src="https://cdn.millionluxury.com/image-resizing?image=https://maustorageprod.blob.core.windows.net/spinfileuat/Spin/Data/Estate/IMG/ceb693ad6b7643fc8c1be271d6a9c068.svg"
           alt="Logo"
-          className="h-8 w-auto filter brightness-0"
+          width={120}
+          height={32}
+          className="invert"
+          unoptimized
         />
       </div>
     </header>
