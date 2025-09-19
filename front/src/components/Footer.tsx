94f2b064bb3e8718cb04cd62e7c0cd0857ae32c4
Jonathan <cifuentes.jonathan@gmail.com>
2025-09-19 12:40:35 -0500

    feat: layout with header & footer

---
+++ front/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-black border-t border-gray-700">
+      <div className="container mx-auto px-4 py-4 flex justify-center">
+        <img
+          src="https://cdn.millionluxury.com/image-resizing?image=https://maustorageprod.blob.core.windows.net/spinfileuat/Spin/Data/Estate/IMG/ceb693ad6b7643fc8c1be271d6a9c068.svg"
+          alt="Logo"
+          className="h-8 w-auto filter brightness-0 invert"
+        />
+      </div>
+    </footer>
+  );
+}
---
+++ front/src/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="w-full bg-white border-b border-gray-200">
+      <div className="container mx-auto px-4 py-4">
+        <img
+          src="https://cdn.millionluxury.com/image-resizing?image=https://maustorageprod.blob.core.windows.net/spinfileuat/Spin/Data/Estate/IMG/ceb693ad6b7643fc8c1be271d6a9c068.svg"
+          alt="Logo"
+          className="h-8 w-auto filter brightness-0"
+        />
+      </div>
+    </header>
+  );
+}
